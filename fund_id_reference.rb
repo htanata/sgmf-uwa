1da73d92d2b6031081a0f2ec621d346b6a9caf03
htanata <htanata@1897b337-5133-0410-9e0c-ef95563ced0c>
2009-04-28 10:53:43 +0000

    Add script to generate fund ID reference wiki.

---
+++ fund_id_reference.rb
@@ -0,0 +1,24 @@
+# Generates Fund ID Reference page for the wiki at
+# http://code.google.com/p/sgmf-uwa/wiki/FundIdReference
+
+require 'rubygems'
+require 'open-uri'
+require 'nokogiri'
+
+doc = Nokogiri::HTML(open('http://www.fundsupermart.com/main/home/index.svdo'))
+funds = doc.css('select.fundselect option')
+
+output = "= Fund ID Reference =\n"
+output << "_As of #{Time.now.strftime('%d %b %Y')}_\n\n"
+output << "|| *Fund Name* || *Fund ID* ||\n"
+
+funds.drop(1).each do |fund|
+  output << '|| '
+  output << fund.content
+  output << ' || '
+  output << fund.attribute('value').value.gsub(/^.*=/, '')
+  output << " ||\n"
+end
+
+puts output
+
